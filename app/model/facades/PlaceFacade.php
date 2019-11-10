b29b3b2ff594a31e9aedb5de78924c187937597d
pehus <info@pehus.cz>
2019-11-11 00:09:26 +0100

    Update PlaceFacade.php
    creating method place is free

--- app/model/facades/PlaceFacade.php
+++ app/model/facades/PlaceFacade.php
@@ -2,10 +2,28 @@
 
 namespace App\Model\Facades;
 
+use Kdyby\Doctrine\EntityManager;
+use App\Model\Entities;
+use Nette\Utils\DateTime;
+
 class PlaceFacade 
 {
     const PLACES = 240;
-
+    
+    /**
+     * Manager for works with entities
+     * @var EntityManger $entityManager
+     */
+    private $entityManager;
+    
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager) 
+    {
+        $this->entityManager = $entityManager;
+    }
+     
     /**
      * get places
      * @param type $date
@@ -16,6 +34,10 @@ class PlaceFacade
         return $this->places();
     }
     
+    /**
+     * create places
+     * @return array
+     */
     private function places(): array
     {
         $places = [];
@@ -27,9 +49,33 @@ class PlaceFacade
         return $places;
     }
     
-    public function isFree($place, $date_from, $date_to)
+    /**
+     * check place is free
+     * @param integer $place
+     * @param Nette\Utils\DateTime $date_from
+     * @param Nette\Utils\DateTime $date_to
+     * @return bool
+     */
+    public function isFree($place, $date_from, $date_to): bool
     {
+        $qb = $this->entityManager->createQueryBuilder();
+        $qb->select('reservation_date')
+            ->from(Entities\ReservationDate::class, 'reservation_date')
+            ->innerJoin(Entities\Reservation::class, 'reservation')
+            ->where('reservation_date.date BETWEEN :date_from AND :date_to')
+            ->andWhere('reservation = :place')
+            ->setParameters([
+                'date_from' => $date_from->format('Y-m-d'),
+                'date_to' => $date_to->format('Y-m-d'), 
+                'place' => $place
+            ]);
         
+        if($qb->getQuery()->getResult())
+        {
+            return false;
+        }
+        
+        return true;
     }
     
 }
